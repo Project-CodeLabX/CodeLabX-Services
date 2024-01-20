c077310d0ee44e6c65cf72ac2f7c47f6ecc4686f
Abhi <abhishekvelekar79200@gmail.com>
2024-01-20 21:04:27 +0530

    initial route test done

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var r = mux.NewRouter()
+
+	r.HandleFunc("/signup", signUp).Methods("POST")
+
+	defer log.Fatal(http.ListenAndServe(":8010", r))
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte("<h1> Hi from CodeLabx</h1>"))
 }
