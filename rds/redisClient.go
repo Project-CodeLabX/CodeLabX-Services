d290e0d98ced26da7cb66b8d7ec552e7ed250083
Abhi <abhishekvelekar79200@gmail.com>
2024-05-08 20:55:25 +0530

    delete client on connection close functionality added wit os redis url

--- rds/redisClient.go
+++ rds/redisClient.go
@@ -3,12 +3,12 @@ package rds
 import "github.com/redis/go-redis/v9"
 
 const (
-	url = "redis://Abhi1060:Abhi1060@localhost:6379/0?protocol=3"
+	url = "redis://52.66.243.237:6379/0?protocol=3"
 )
 
 func GetRedisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "15.206.90.96:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 		Protocol: 3,  // specify 2 for RESP 2 or 3 for RESP 3
